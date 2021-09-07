2a57d5f955a88516bc84391ff903f89423090536
Exjoy23 <exjoy23@gmail.com>
2021-09-07 17:55:30 +0300

    добавит доступность select

--- src/components/select/select.jsx
+++ src/components/select/select.jsx
@@ -8,12 +8,24 @@ const CreditType = {
   MORTGAGE: 'Ипотечное кредитование',
   CAR: 'Автомобильное кредитование',
 };
+const EventCodes = {
+  SPACE: 'Space',
+  ENTER: 'Enter',
+};
 
 function Select({ activeType, onActiveType }) {
   const [isOpen, setIsOpen] = useState(false);
 
-  const onListChange = (evt) => {
-    onActiveType(evt.target.value);
+  const onInputKeydown = (evt) => {
+    if (evt.code === EventCodes.SPACE || evt.code === EventCodes.ENTER) {
+      evt.preventDefault();
+      onActiveType(evt.target.value);
+      setIsOpen(false);
+    }
+  };
+
+  const onLabelClick = (evt) => {
+    onActiveType(evt.target.htmlFor.toUpperCase());
     setIsOpen(false);
   };
 
@@ -27,28 +39,38 @@ function Select({ activeType, onActiveType }) {
         {CreditType[activeType]}
       </button>
       {isOpen && (
-        <ul className={styles.list} onChange={onListChange}>
-          <li className={styles.item} tabIndex="0">
+        <ul className={styles.list}>
+          <li className={styles.item}>
             <input
               className={classNames('visually-hidden', styles.input)}
+              onKeyDown={onInputKeydown}
               id="mortgage"
               type="radio"
               name="credit"
               value="MORTGAGE"
             />
-            <label className={styles.label} htmlFor="mortgage">
+            <label
+              className={styles.label}
+              onClick={onLabelClick}
+              htmlFor="mortgage"
+            >
               {CreditType.MORTGAGE}
             </label>
           </li>
-          <li className={styles.item} tabIndex="0">
+          <li className={styles.item}>
             <input
               className={classNames('visually-hidden', styles.input)}
+              onKeyDown={onInputKeydown}
               id="car"
               type="radio"
               name="credit"
               value="CAR"
             />
-            <label className={styles.label} htmlFor="car">
+            <label
+              className={styles.label}
+              onClick={onLabelClick}
+              htmlFor="car"
+            >
               {CreditType.CAR}
             </label>
           </li>
